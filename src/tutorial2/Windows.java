8937422c445700cd2039c531ef8ffce1dc11bbae
Remigiusz <meystr@users.noreply.github.com>
2018-11-13 07:29:52 +0100

    asd

--- src/tutorial2/Windows.java
+++ src/tutorial2/Windows.java
@@ -1,43 +1,52 @@
 package tutorial2;
 
+import javafx.animation.Animation;
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Windows extends Application {
-    Canvas canvas = new Canvas(90, 75);
-    GraphicsContext gc = canvas.getGraphicsContext2D();
-    Image image = new Image("tutorial2/MS3-TypeR.PNG");
 
 
-    @Override
+    private static final Image IMAGE = new Image("tutorial2/Astyanax.gif");
+
+    private static final int COLUMNS  =   3;
+    private static final int COUNT    =  3;
+    private static final int OFFSET_X =  87;
+    private static final int OFFSET_Y =  3;
+    private static final int WIDTH    = 67;
+    private static final int HEIGHT   = 88;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     public void start(Stage primaryStage) {
-        System.out.println("Metoda start :)");
-        Group root = new Group();
-        Scene scene = new Scene(root,600,500);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Próba okien :)");
-       // primaryStage.setMaximized(true);
-        primaryStage.sizeToScene();
-        primaryStage.centerOnScreen();
+        primaryStage.setTitle("The Horse in Motion");
+
+        final ImageView imageView = new ImageView(IMAGE);
+        imageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+
+        final Animation animation = new SpriteAnimation(
+                imageView,
+                Duration.millis(1000),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT
+        );
+        animation.setCycleCount(Animation.INDEFINITE);
+        animation.play();
+
+        primaryStage.setScene(new Scene(new Group(imageView)));
+        imageView.setX(300);
+        imageView.setY(500);
+        primaryStage.setMinWidth(1200);
+        primaryStage.setMinHeight(800);
         primaryStage.show();
-        root.getChildren().add(canvas);
-        gc.drawImage(image, 20, 10);
-    }
-
-    @Override
-    public void init() throws Exception {
-        super.init();
-        System.out.println("Metoda init :)");
-    }
-
-    @Override
-    public void stop() throws Exception {
-        super.stop();
-        System.out.println("Metoda stop :)");
     }
 }
